412a6d5ad0421be9cee8afc4acdde96312923398
Alex <nikitin2009@gmail.com>
2019-05-30 18:32:39 +0300

    Add tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   describe "validations" do
 
     let(:valid_user) { FactoryBot.build(:user) }
-    let(:invalid_user) { FactoryBot.build(:user, first_name: nil) }
+    let(:invalid_first_name) { FactoryBot.build(:user, first_name: nil) }
+    let(:invalid_last_name) { FactoryBot.build(:user, last_name: nil) }
 
     context "complete user details" do
       it "is valid" do
@@ -13,10 +15,44 @@ RSpec.describe User, type: :model do
     end
 
     context "incomplete user details" do
-      it "is not valid" do
-        expect(invalid_user).to be_invalid
+      it "is invalid without a first name" do
+        expect(invalid_first_name).to be_invalid
+      end
+      it "is invalid without a last name" do
+        expect(invalid_last_name).to be_invalid
       end
     end
 
+    it "does not allow duplicate emails" do
+      User.create!(
+        first_name: "Test",
+        last_name: "test",
+        email: "test@mail.com",
+        password: "123456"
+      )
+      new_user = User.new(email: "test@mail.com")
+      new_user.valid?
+      expect(new_user.errors[:email]).to include("has already been taken")
+    end
   end
+
+  describe "instance methods" do
+    let(:john) { FactoryBot.create(:user) }
+    let(:mike) { FactoryBot.create(:user) }
+    let(:charles) { FactoryBot.create(:user) }
+    let(:rachel) { FactoryBot.create(:user) }
+
+    before do
+      FactoryBot.create(:friendship, active_friend: john, passive_friend: mike)
+      FactoryBot.create(:friendship, active_friend: charles, passive_friend: john)
+      FactoryBot.create(:friendship, active_friend: rachel, passive_friend: john)
+    end
+
+    describe "#friends" do
+      it "returns a user's complete set of friends" do
+        expect(john.friends.count).to be(3)
+      end
+    end
+  end
+
 end
