97c0aee8994e09625c285b5992964f5b5e351592
Alex <nikitin2009@gmail.com>
2019-06-04 10:02:04 +0300

    Fix issue #8

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,4 @@
 class Like < ApplicationRecord
-  validates :user, presence: true
-  validates :post, presence: true
-
-  validates :user_id, uniqueness: { scope: :post_id }
-
   belongs_to :user
   belongs_to :post
 end
