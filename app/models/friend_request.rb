6ae982aa3e66a3a3fc010638bf52341d9edaa7ae
Alex <nikitin2009@gmail.com>
2019-06-04 17:35:49 +0300

    Add 'existing request' constraint to FriendRequest (closes #6)

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,4 +1,14 @@
 class FriendRequest < ApplicationRecord
+  validate :cannot_send_request_when_received_from_same_user
+
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
+
+  private
+    def cannot_send_request_when_received_from_same_user
+      received = FriendRequest.find_by(receiver: sender)
+      if received && received.sender == receiver
+        errors.add(:duplicate, "there is a request between the two users")
+      end
+    end
 end
--- spec/models/friend_request_spec.rb
+++ spec/models/friend_request_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe FriendRequest, type: :model do
     end
   end
 
+  describe "validations" do
+    it "ensures uniqueness of pair [sender, receiver] / [receiver, sender]" do
+      user1 = FactoryBot.create(:user)
+      user2 = FactoryBot.create(:user)
+      request_from_1_to_2 = FriendRequest.create(sender: user1, receiver: user2)
+      request_from_2_to_1 = FriendRequest.new(sender: user2, receiver: user1)
+      expect(request_from_2_to_1.valid?).to be false
+    end
+  end
+
 end
