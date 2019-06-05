38ded65538ca110b73095ccc74f6d4a6335255de
Alex <nikitin2009@gmail.com>
2019-06-05 12:14:47 +0300

    Add 'existing friendship' constraint to Friendship (closes #7)

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,13 @@
 class Friendship < ApplicationRecord
   belongs_to :active_friend, class_name: "User"
   belongs_to :passive_friend, class_name: "User"
+
+  before_save :check_inverse
+
+  private
+    def check_inverse
+      return unless Friendship.exists?(active_friend_id: passive_friend.id, passive_friend_id: active_friend.id)
+
+      self.active_friend, self.passive_friend = passive_friend, active_friend
+    end
 end
